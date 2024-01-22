806937f9e050f66c924dec9451683ee4b0c1f915
preyero <prl.sahelices@gmail.com>
2024-01-22 18:36:29 +0000

    interrater reliability

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import pandas as pd
 
 import scripts.dataCollect as dc
 from scripts.dataCollect import load_hateRep
+from scripts.agreement import get_alpha_and_deltas
 
 PROJ_DIR = os.getcwd()
 U_PATH = os.path.join(PROJ_DIR, 'annotators')
@@ -17,14 +19,47 @@ data, samples, users = load_hateRep(u_path=U_PATH, d_path=D_PATH)
 print('Imported data with samples, annotations, and user tables')
 
 
-# TODO: two phases with user info and context
-
 ################################################
 # Agreement
 ################################################
 
+example = data[['Question ID', 'User', 'gender_1']].sample(10)
+print(example)
 
+# TABLE 1: including all annotations, Krippendorff's Alpha scores and delta between phases
+tables_1, table_1_cols = {}, ['Ph1', 'Ph2', 'Δ']
+# from gender and sexuality binary categories
+for g in dc.TARGET_GROUPS:
+    values = {}
+    for sg in dc.TARGET_LABELS[g]:
+        values[sg] = get_alpha_and_deltas(data, sg)
+    tables_1[g] = pd.DataFrame.from_dict(values, orient='index', columns=table_1_cols)
 
+# from the other data annotations
+values = {}
+for label in dc.TARGET_GROUPS + dc.HATE_QS:
+    values[label] = get_alpha_and_deltas(data, label)
+tables_1['Other'] = pd.DataFrame.from_dict(values, orient='index', columns=table_1_cols)
+
+[tables_1[t].sort_values(by='Δ', inplace=True) for t in tables_1.keys()]
+
+# TABLE 2: Krippendorff's Alpha delta scores disaggregated by annotator demographics
+tables_2 = {}
+for g in dc.TARGET_GROUPS:
+    values = {}
+    for c in dc.CATEG.values():
+        print(data[c].value_counts())
+        for sc in data[c].unique():
+            # for every value in the category
+            subset, deltas = data.loc[data[c] == sc], []
+            for sg in dc.TARGET_LABELS[g]:
+                deltas.append(round(get_alpha_and_deltas(subset, sg)[-1], 3))
+            values[sc] = deltas
+    tables_2[g] = pd.DataFrame.from_dict(values)
+    tables_2[g].index = dc.TARGET_LABELS[g]
+    # same order as tables 1
+    tables_2[g] = tables_2[g].reindex(tables_1[g].index.to_list())
+            
 
 ################################################
 # Confidence
---
+++ scripts/agreement.py
@@ -0,0 +1,50 @@
+import numpy as np 
+import pandas as pd
+import krippendorff
+
+#########################
+# Inter-annotator agreement (IAA)/Interrater reliability
+#########################
+
+# 1. Cohen's Kappa: agreement between two raters
+# https://www.surgehq.ai/blog/inter-rater-reliability-metrics-understanding-cohens-kappa
+from sklearn.metrics import cohen_kappa_score
+# y1: label assigned by first rater (i think is nominal, a variable with n_classes) 
+# y2: label by rater 2
+# only in the intersect between two raters!
+#from statsmodels.stats.inter_rater import cohens_kappa
+# For more than two raters, it can create a contigency of n_raters-dimensions (instead of 2-dimen)
+# https://www.statsmodels.org/stable/generated/statsmodels.stats.inter_rater.to_table.html
+# sample x annotations. BUT no NaN (have to delete them?? i think it does use the information of which labeler for p_e.
+# look kappa with missing data (at random): listwise deletion https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6506991/ 
+# Maybe one against the others? gives one value per annotator.
+
+# 2. Fleiss' Kappa: agreement between more than 3 raters (nominal: x in {A, B, C}) > 
+# Kendall for ordinal (A<B<C), intra-class correlation for a metric.
+# https://datatab.net/tutorial/fleiss-kappa
+#from statsmodels.stats.inter_rater import fleiss_kappa
+# https://www.statsmodels.org/stable/generated/statsmodels.stats.inter_rater.aggregate_raters.html
+# from counts of category assigned: Fleiss Kappa can be specially used when participants are rated by different sets of raters. This means that the raters responsible for rating one subject are not assumed to be the same as those responsible for rating another (Fleiss et al., 2003).
+
+# 3. Krippendorf's Alpha: incomplete data (not every annotator each sample) and arbitrary number of raters (not always 2 or 3)
+# https://www.surgehq.ai/blog/inter-rater-reliability-metrics-an-introduction-to-krippendorffs-alpha
+# https://www.lighttag.io/blog/krippendorffs-alpha/
+def krippendorf(df: pd.DataFrame, rater_col: str, subject_col: str, rating_col: str):
+
+    print(f'computing Krippendorf on {rating_col}')
+    # Transform to data format, e.g.,
+    # rating_table = [[np.nan, np.nan, np.nan, np.nan, np.nan, 3, 4, 1, 2, 1, 1, 3, 3, np.nan, 3], # User 1
+    #                 [1, np.nan, 2, 1, 3, 3, 4, 3, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], # User 2
+    #                 [np.nan, np.nan, 2, 1, 3, 4, 4, np.nan, 2, 1, 1, 3, 3, np.nan, 4]] # User 3
+    df = df.pivot_table(index=rater_col, columns=subject_col, values=rating_col, aggfunc="first", fill_value=np.nan)
+    rating_table = df.values.tolist()
+
+    
+    # Compute Krippendorf's Alpha
+    return krippendorff.alpha(reliability_data=rating_table, level_of_measurement="nominal")
+
+def get_alpha_and_deltas(data_subset: pd.DataFrame, label: str):
+    """ Krippendorff's Alpha in both phases and delta between them """
+    alpha_1 = krippendorf(df=data_subset, rater_col='User', subject_col='Question ID', rating_col=f'{label}_1')
+    alpha_2 = krippendorf(df=data_subset, rater_col='User', subject_col='Question ID', rating_col=f'{label}_2')
+    return [round(alpha_1, 3), round(alpha_2, 3), round(alpha_2-alpha_1, 3)]
