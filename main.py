af8f7a5c4b39e623e7b37519b056e8b336b38338
preyero <prl.sahelices@gmail.com>
2024-01-29 17:56:07 +0000

    ... fleiss kappa

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 import scripts.dataCollect as dc
 from scripts.dataCollect import load_hateRep
-from scripts.agreement import get_alphas_and_delta
+from scripts.agreement import get_scores_and_delta
 from scripts.utils import export_table_plot
+from scripts.agreement import keep_by_annotation_count
 
 PROJ_DIR = os.getcwd()
 U_PATH = os.path.join(PROJ_DIR, 'annotators')
@@ -21,11 +22,12 @@ print('Imported data with samples, annotations, and user tables')
 
 
 ################################################
-# Agreement
+# Agreement: Krippendorff's Alpha
 ################################################
 
 example = data[['Question ID', 'User', 'gender_1']].sample(10)
 print(example)
+print('... unique texts', len(data['Question ID'].unique()))
 
 # TABLE 1: including all annotations, Krippendorff's Alpha scores and delta between phases
 tables_1, table_1_cols = {}, ['Ph1', 'Ph2', 'Δ']
@@ -33,12 +35,12 @@ tables_1, table_1_cols = {}, ['Ph1', 'Ph2', 'Δ']
 for g in dc.TARGET_GROUPS:
     values = {}
     for sg in dc.TARGET_LABELS[g]:
-        values[sg] = get_alphas_and_delta(data, sg)
+        values[sg] = get_scores_and_delta(data, 'krippendorf', sg)
     tables_1[g] = pd.DataFrame.from_dict(values, orient='index', columns=table_1_cols)
 # from the other data annotations
 values = {}
 for label in dc.TARGET_GROUPS + dc.HATE_QS:
-    values[label] = get_alphas_and_delta(data, label)
+    values[label] = get_scores_and_delta(data, 'krippendorf', label)
 tables_1['other'] = pd.DataFrame.from_dict(values, orient='index', columns=table_1_cols)
 # sort values by delta
 [tables_1[t].sort_values(by='Δ', inplace=True) for t in tables_1.keys()]
@@ -56,29 +58,51 @@ for g in dc.TARGET_GROUPS:
             # for every value in the category
             subset, alphas_2, deltas = data.loc[data[c] == sc], [], []
             for sg in dc.TARGET_LABELS[g]:
-                _, alpha_2, delta = get_alphas_and_delta(subset, sg)
+                _, alpha_2, delta = get_scores_and_delta(subset, 'krippendorf', sg)
                 alphas_2.append(alpha_2)
                 deltas.append(delta)
             values_alpha[sc], values_delta[sc] = alphas_2, deltas
     tables_2[f'{g}_alpha'], tables_2[f'{g}_delta'] = pd.DataFrame.from_dict(values_alpha), pd.DataFrame.from_dict(values_delta)
-    # TODO: of personal experiences
+    # TODO: of personal experiences (bootstrapping?)
     # index labels and same order as tables 1
     for t in ['alpha', 'delta']: 
         tables_2[f'{g}_{t}'].index = dc.TARGET_LABELS[g]
         tables_2[f'{g}_{t}'] = tables_2[f'{g}_{t}'].reindex(tables_1[g].index.to_list())
 
     # Table plots
-    # TODO: change 'g1'/'g2' coding to 'non-LGBTQ/LGBT'
-    # TODO: also do table with personal experience (of yes)
-    cols = ['g1', 'g2', 'M', 'W', 'S', 'G']
-    plot_1, plot_2 = ['g1', 'M', 'W',], ['g2', 'S', 'G']
+    # TODO: also do table with personal experience (of yes/no, and by 4 subcategories)
+    cols = ['nonLGBT', 'LGBT', 'M', 'W', 'S', 'G']
+    # plot_1, plot_2 = ['nonLGBT', 'M', 'W',], ['LGBT', 'S', 'G']
     export_table_plot(cell_values_df=tables_2[f'{g}_alpha'][cols], color_values_df=tables_2[f'{g}_delta'][cols], pdf_filename=f'figures/krippendorff_{g}_{'_'.join(cols)}.pdf')
-    export_table_plot(cell_values_df=tables_2[f'{g}_alpha'][plot_1], color_values_df=tables_2[f'{g}_delta'][plot_1], pdf_filename=f'figures/krippendorff_{g}_{'_'.join(plot_1)}.pdf')
-    export_table_plot(cell_values_df=tables_2[f'{g}_alpha'][plot_2], color_values_df=tables_2[f'{g}_delta'][plot_2], pdf_filename=f'figures/krippendorff_{g}_{'_'.join(plot_2)}.pdf')
+    # export_table_plot(cell_values_df=tables_2[f'{g}_alpha'][plot_1], color_values_df=tables_2[f'{g}_delta'][plot_1], pdf_filename=f'figures/krippendorff_{g}_{'_'.join(plot_1)}.pdf')
+    # export_table_plot(cell_values_df=tables_2[f'{g}_alpha'][plot_2], color_values_df=tables_2[f'{g}_delta'][plot_2], pdf_filename=f'figures/krippendorff_{g}_{'_'.join(plot_2)}.pdf')
     
-        
-        
-            
+################################################
+# Agreement: Fleiss Kappa
+################################################ 
+# Case a: keep only those with 6 annotations
+d_filter = keep_by_annotation_count(df=data, by='Question ID', n_counts=6, method='filter') 
+print('... unique texts', len(d_filter['Question ID'].unique()))
+# Case b: undersample to 5 annotations  
+d_downsample = ... 
+
+tables_1_kappa = {}
+# from gender and sexuality binary categories
+for g in dc.TARGET_GROUPS:
+    val_filter = {}
+    for sg in dc.TARGET_LABELS[g]:
+        val_filter[sg] = get_scores_and_delta(d_filter, 'fleiss', sg)
+    tables_1_kappa[f'{g}_filter'] = pd.DataFrame.from_dict(val_filter, orient='index', columns=table_1_cols)
+# from the other data annotations
+val_filter = {}
+for label in dc.TARGET_GROUPS + dc.HATE_QS:
+    val_filter[label] = get_scores_and_delta(d_filter, 'fleiss', label)
+tables_1_kappa['other_filter'] = pd.DataFrame.from_dict(val_filter, orient='index', columns=table_1_cols)
+# sort values by delta
+[tables_1_kappa[t].sort_values(by='Δ', inplace=True) for t in tables_1_kappa.keys()]
+          
+
+print('')
 
 ################################################
 # Confidence
--- scripts/agreement.py
+++ scripts/agreement.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import pandas as pd
+from statsmodels.stats.inter_rater import fleiss_kappa, aggregate_raters
 import krippendorff
+
 
 #########################
 # Inter-annotator agreement (IAA)/Interrater reliability
@@ -25,26 +27,63 @@ from sklearn.metrics import cohen_kappa_score
 #from statsmodels.stats.inter_rater import fleiss_kappa
 # https://www.statsmodels.org/stable/generated/statsmodels.stats.inter_rater.aggregate_raters.html
 # from counts of category assigned: Fleiss Kappa can be specially used when participants are rated by different sets of raters. This means that the raters responsible for rating one subject are not assumed to be the same as those responsible for rating another (Fleiss et al., 2003).
+def keep_by_annotation_count(df: pd.DataFrame, by: str, n_counts: int, method: str):
+    # Get counts
+    c = df.groupby(by)[by].transform('count')
+
+    if method == 'filter':
+        return df[ c == n_counts ]
+    elif method == 'downsample':
+        return
+
+def fleiss(df: pd.DataFrame, subject_col: str, rating_col: str, verbose: bool = False):
+    # Table of category assignments, e.g. (3 raters),
+    # category_assignment = [[0, 0, 1], # Text/Subject 1
+    #                 [1, 0, 0], # Text/Suject 2
+    #                 [1, 0, 0]] # Text/Subject 3
+    category_assignment = []
+    subject_ids = df[subject_col].unique()
+    for subject_id in subject_ids:
+        ratings = df.loc[df[subject_col]==subject_id, rating_col].values.tolist()
+        category_assignment.append(ratings)
+    
+    # Format for fleiss_kappa, e.g., 
+    # table = [[1, 2], # Text/Subject 1
+    #                 [2, 1], # Text/Suject 2
+    #                 [2, 1]] # Text/Subject 3
+    table, categories = aggregate_raters(data=category_assignment)
+    if verbose:
+        print(f'Computing Fleiss kappa on {rating_col} with categories: {categories}')
+
+    # Compute fleiss kappa score
+    return fleiss_kappa(table)
 
 # 3. Krippendorf's Alpha: incomplete data (not every annotator each sample) and arbitrary number of raters (not always 2 or 3)
 # https://www.surgehq.ai/blog/inter-rater-reliability-metrics-an-introduction-to-krippendorffs-alpha
 # https://www.lighttag.io/blog/krippendorffs-alpha/
-def krippendorf(df: pd.DataFrame, rater_col: str, subject_col: str, rating_col: str):
+def krippendorf(df: pd.DataFrame, rater_col: str, subject_col: str, rating_col: str, verbose: bool = False):
 
-    print(f'computing Krippendorf on {rating_col}')
+    if verbose:
+        print(f'computing Krippendorf on {rating_col}')
     # Transform to data format, e.g.,
-    # rating_table = [[np.nan, np.nan, np.nan, np.nan, np.nan, 3, 4, 1, 2, 1, 1, 3, 3, np.nan, 3], # User 1
-    #                 [1, np.nan, 2, 1, 3, 3, 4, 3, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], # User 2
-    #                 [np.nan, np.nan, 2, 1, 3, 4, 4, np.nan, 2, 1, 1, 3, 3, np.nan, 4]] # User 3
+    # rating_table = [[np.nan, np.nan, np.nan, np.nan, np.nan, 3, 4, 1, 2, 1, 1, 3, 3, np.nan, 3], # User/Rater 1
+    #                 [1, np.nan, 2, 1, 3, 3, 4, 3, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan], # User/Rater 2
+    #                 [np.nan, np.nan, 2, 1, 3, 4, 4, np.nan, 2, 1, 1, 3, 3, np.nan, 4]] # User/Rater 3
     df = df.pivot_table(index=rater_col, columns=subject_col, values=rating_col, aggfunc="first", fill_value=np.nan)
     rating_table = df.values.tolist()
 
-    
     # Compute Krippendorf's Alpha
     return krippendorff.alpha(reliability_data=rating_table, level_of_measurement="nominal")
 
-def get_alphas_and_delta(data_subset: pd.DataFrame, label: str):
-    """ Krippendorff's Alpha in both phases and delta between them """
-    alpha_1 = krippendorf(df=data_subset, rater_col='User', subject_col='Question ID', rating_col=f'{label}_1')
-    alpha_2 = krippendorf(df=data_subset, rater_col='User', subject_col='Question ID', rating_col=f'{label}_2')
-    return [round(alpha_1, 3), round(alpha_2, 3), round(alpha_2-alpha_1, 3)]
+def get_scores_and_delta(data_subset: pd.DataFrame, score: str, rating_col: str, rater_col: str = 'User', subject_col: str = 'Question ID', verbose: bool = False):
+    """ IAA in both phases and delta between them """
+
+    # get alpha values
+    if score == 'krippendorf':
+        val_1 = krippendorf(df=data_subset, rater_col=rater_col, subject_col=subject_col, rating_col=f'{rating_col}_1', verbose=verbose)
+        val_2 = krippendorf(df=data_subset, rater_col=rater_col, subject_col=subject_col, rating_col=f'{rating_col}_2', verbose=verbose)
+    # get kappa values
+    elif score == 'fleiss':
+        val_1 = fleiss(df=data_subset, subject_col=subject_col, rating_col=f'{rating_col}_1', verbose=verbose)
+        val_2 = fleiss(df=data_subset, subject_col=subject_col, rating_col=f'{rating_col}_2', verbose=verbose)
+    return [round(val_1, 3), round(val_2, 3), round(val_2-val_1, 3)]
