fc844ea4d957802c690b8e205ebdcaa06c5f1a79
preyero <prl.sahelices@gmail.com>
2024-04-12 17:51:20 +0100

    ... simplify columns used

--- scripts/dataCollect.py
+++ scripts/dataCollect.py
@@ -5,34 +5,6 @@ import pandas as pd
 
 # Dataset features
 CATEG = {'c1': 'group', 'c2': 'subgroupA', 'c3': 'subgroupB'}
-FACT = {
-    'f0': 'Lgbtq+',
-    # sexuality
-    'f1': 'Sexual orientation', 
-    # sex and gender
-    'f2': 'Cisgender and transgender', 'f3': 'Gender', 'f4': 'Sex'
-    }
-COFACT = {
-    # annotator performance
-    'f0': 'Time taken', 'f0.1_1': 'Complete_1_M', 'f0.1_2': 'Complete_2_M', 
-    'f1':'Total approvals', 
-    # annotator's background
-    'f2':'Age', 
-    'f3': 'Language', 
-    'f4.1':'Country of birth', 'f4.2':'Country of residence', 'f4.3':'Ethnicity simplified', 'f4.4':'Nationality', 
-    'f5.1':'Employment status', 'f5.2': 'Student status',
-    # annotator's experience
-    'f6': 'Personal Experience', 'f6.1': 'personally', 'f6.2': 'witnessed', 'f6.3': 'unsure', 'f6.4': 'none',
-    # annotator and reasoning
-    'f7': 'User', 'f7.1': 'Justify Gender', 'f7.2': 'Justify Sexuality',
-    'f8': 'justify_change', 'f8.1': 'justify_change_gender', 'f8.2': 'justify_change_sexuality',
-    # semantics
-    'f9': 'context',
-    'f8': 'context_accept', 'f8.1': 'context_accept_gender', 'f8.2': 'context_accept_sexuality',
-    # data provenance
-    'f9': 'dataset', 
-    'f10': 'Question ID',
-    }
 
 # Experiment setup
 PHASES = ['1', '2']
@@ -45,7 +17,7 @@ TARGET_GROUPS = ['gender', 'sexuality']
 TARGET_LABELS = {}
 
 def import_users(u_path: str):
-    """ Import annotator tables from (hateRep/annotators) folder """
+    """ Import Prolific tables from (hateRep/annotators) folder """
 
     # Import prolific tables using info from phase 1 
     f_users, users = glob.glob(f'{u_path}/*p1_prolific*'), []
@@ -85,8 +57,6 @@ def import_survey(d_path: str):
         for t in ['Started', 'Finished']:
            questions[f'Phase {p} {t}'] = questions[f'Phase {p} {t}'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S")) 
         questions[f'Complete_{p}'] = questions[f'Phase {p} Finished'] - questions[f'Phase {p} Started'] 
-        questions[f'Complete_{p}_M'] = questions[f'Complete_{p}'].apply(lambda x: round(x.total_seconds()/60), 2) 
-
 
     return samples, annot, questions
 
@@ -100,8 +70,9 @@ def scale_encoding(value: str, scale: List[str]) -> float:
     elif value == scale[2]:
         return 1
     
+
 def one_hot_encoding(df: pd.DataFrame, col: str):
-    """ Expand a df with binary encodings of column with list of string values """
+    """ Expand a dataframe with binary encodings of column with list of string values """
 
     from sklearn.preprocessing import MultiLabelBinarizer
     mlb = MultiLabelBinarizer()
@@ -110,15 +81,15 @@ def one_hot_encoding(df: pd.DataFrame, col: str):
 
 
 def stemmatize(text: str) -> List[str]:
-    """ tokenize, lower-case, and stem filter """
+    """ Tokenize, lower-case, and stem filter an input string """
     from whoosh.analysis import StemmingAnalyzer
     stemmer = StemmingAnalyzer(stoplist=None) 
     return [token.text for token in stemmer(text)]
 
 
 def excOuterJoin(reasons1: str, reasons2: str, verbose: bool = False) -> List[str]:
-    """ Returns exclusive full outer join """
-    # TOkenize
+    """ Exclusive full outer join: obtain words used to justify target groups only in one phase  """
+    # tokenize
     tokens1 = stemmatize(reasons1)
     tokens2 = stemmatize(reasons2)
     # return not in intersection
@@ -127,17 +98,6 @@ def excOuterJoin(reasons1: str, reasons2: str, verbose: bool = False) -> List[st
         print(f"{tokens1}|{tokens2}->{not_intersection}")
     return not_intersection
 
-
-def innerJoin(reasons1: str, reasons2: str, verbose: bool = False) -> List[str]:
-    """ Returns intersection """
-    # TOkenize
-    tokens1 = stemmatize(reasons1)
-    tokens2 = stemmatize(reasons2)
-    # return in intersection
-    intersection = list(set(tokens1) & set(tokens2))
-    if verbose:
-        print(f"{tokens1}∩{tokens2}->{intersection}")
-    return intersection
 
 
 def load_hateRep(u_path: str, d_path: str):
@@ -186,15 +146,10 @@ def load_hateRep(u_path: str, d_path: str):
 
     # merge by phases 
     annot = pd.merge(annot.loc[annot.Phase==1], annot.loc[annot.Phase==2], on=['User', 'Question ID', 'Question'], how='inner', suffixes=['_1', '_2'])
-    # and with semantic context
     annot = pd.merge(samples.drop(columns=['Question']), annot, on=['Question ID'], how='inner')
     for g in TARGET_GROUPS:
         # change in justifications across phase
         annot[f'justify_change_{g}'] = annot.apply(lambda x: ', '.join(excOuterJoin(x[f'Justify {g.capitalize()}_1'], x[f'Justify {g.capitalize()}_2'])), axis=1)
-        # get if context explains the change
-        annot[f'context_accept_{g}'] = annot.apply(lambda x: innerJoin(x[f'justify_change_{g}'], x[f'context']), axis=1)
-    annot['justify_change'] = annot.apply(lambda x: 0 if x['justify_change_gender'] + x['justify_change_sexuality'] == '' else 1, axis = 1)
-    annot['context_accept'] = annot.apply(lambda x: 0 if len(x['context_accept_gender']) + len(x['context_accept_sexuality']) == 0 else 1, axis = 1)
     
     # Final dataset
     data = pd.merge(annot, users, on='User', how='inner')
